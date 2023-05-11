1e631be1fc67de02577c352f4b91313434c72f66
beltrewilton <beltre.wilton@gmail.com>
2023-05-11 09:58:09 -0400

    Google Colab version.

--- server/main.py
+++ server/main.py
@@ -1,8 +1,12 @@
 import os
+import sys
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 import server.core.app_main as core
+
+IN_COLAB = 'google.colab' in sys.modules
+APP_PORT: int = 8770
 
 base_path = os.getcwd()
 key_pem = os.getcwd() + '/certs/key.pem'
@@ -30,16 +34,18 @@ app.add_middleware(
 async def root():
     return {'message': 'Message from root.'}
 
-
-if __name__ == "__main__":
-    # uvicorn.run(app, host="0.0.0.0", port=8770, ssl_keyfile=key_pem, ssl_certfile=public_pem)
-
+if IN_COLAB:
     # Google Colab version.
     import uvicorn
     from pyngrok import ngrok
     import nest_asyncio
 
-    ngrok_tunnel = ngrok.connect(8770)
+    ngrok_tunnel = ngrok.connect(APP_PORT)
     print('Public URL:', ngrok_tunnel.public_url)
     nest_asyncio.apply()
-    uvicorn.run(app, port=8770)
+    uvicorn.run(app, port=APP_PORT)
+else:
+    if __name__ == "__main__":
+        uvicorn.run(app, host="0.0.0.0", port=APP_PORT, ssl_keyfile=key_pem, ssl_certfile=public_pem)
+
+
